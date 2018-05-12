aa06562c80837b39e61e9b1acb215a0681378165
Thomas Gravekamp <me@thomas-gravekamp.nl>
2018-05-12 21:08:19 +0200

    Make all templates consistent with each other.

--- src/main.c
+++ src/main.c
@@ -10,18 +10,18 @@ int main (void) {
     // Turn on the GPIOC peripheral
     RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
 
-    // Put the pin in general purpose push-pull mode (B00)
+    // Put pin 13 in general purpose push-pull mode
     GPIOC->CRH &= ~(GPIO_CRH_CNF13);
-    // Set the output mode to max. 2MHz (B10)
+    // Set the output mode to max. 2MHz
     GPIOC->CRH |= GPIO_CRH_MODE13_1;
 
     while (1) {
-        // Reset the bit for port C13
+        // Reset the state of pin 13 to output low
         GPIOC->BSRR = GPIO_BSRR_BR13;
 
         delay(500);
 
-        // Set the bit for port C13
+        // Set the state of pin 13 to output high
         GPIOC->BSRR = GPIO_BSRR_BS13;
 
         delay(500);
